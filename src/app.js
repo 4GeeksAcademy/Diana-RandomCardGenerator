c223ea07d9453b76ad057d2dc8a8f7f3050a4d45
Diana Chacón <diianachm1802@gmail.com>
2023-06-27 04:48:07 +0000

    Primer avance de proyecto, funcionalidad casi completa

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#suits").innerHTML = generateSuit();
+    document.querySelector("#number").innerHTML = generateNumber();
+  });
+};
+
+let generateSuit = () => {
+  const allSuits = ["&spades;", "&#9830;", "&clubs;", "&diams;"];
+  const suitsIndex = Math.floor(Math.random() * allSuits.length);
+  const suitsGenerator = allSuits[suitsIndex];
+  return suitsGenerator;
+};
+
+let generateNumber = () => {
+  const allNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const numbersIndex = Math.floor(Math.random() * allNumbers.length);
+  const numbersGenerator = allNumbers[numbersIndex];
+  return numbersGenerator;
 };
