377440a24900ac0416450d11ab7a1c93edbbe395
Diana Chacón <diianachm1802@gmail.com>
2023-06-28 02:00:10 +0000

    Version 0.1.0 del random card generator finalizada

--- src/app.js
+++ src/app.js
@@ -5,20 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let intervalId;
+
 window.onload = () => {
-  document.querySelector("#button").addEventListener("click", () => {
-    document.querySelector("#suits").innerHTML = generateSuit();
-    document.querySelector("#number").innerHTML = generateNumber();
+  document.querySelector("#button").addEventListener("click", cardGenerator);
+  intervalId = setInterval(cardGenerator, 10000);
+};
+
+//generador de carta aleatoria
+let cardGenerator = () => {
+  const suit = generateSuit();
+  const suits = document.querySelectorAll("#suit");
+  const numbers = document.querySelectorAll("#number");
+
+  suits.forEach(suitElement => {
+    suitElement.innerHTML = suit;
+  });
+
+  numbers.forEach(number => {
+    number.innerHTML = generateNumber();
   });
 };
 
+//generador de palo aleatorio
 let generateSuit = () => {
-  const allSuits = ["&spades;", "&#9830;", "&clubs;", "&diams;"];
+  const allSuits = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
   const suitsIndex = Math.floor(Math.random() * allSuits.length);
   const suitsGenerator = allSuits[suitsIndex];
-  return suitsGenerator;
+
+  if (suitsGenerator === "&hearts;" || suitsGenerator === "&diams;") {
+    return `<span class="redColor">${suitsGenerator}</span>`;
+  } else {
+    return suitsGenerator;
+  }
 };
 
+//generador de número aleatorio
 let generateNumber = () => {
   const allNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   const numbersIndex = Math.floor(Math.random() * allNumbers.length);
